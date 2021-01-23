1b3da57547df67c03b4e8e5f06c1e074a75267af
krystalmm <taeyeon_aina@live.jp>
2021-01-23 10:31:50 +0900

    [add] bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # データベースに保存される前に全ての文字列を小文字に変換する！（大文字と小文字を区別しないため！）
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 30 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freez
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  has_secure_password
 end
