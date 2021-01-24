0096dd63bafcbecdf516f855d435a68eb44cceb0
krystalmm <taeyeon_aina@live.jp>
2021-01-24 18:39:40 +0900

    [add] jquery-autokana to signup form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,9 @@ require("@rails/activestorage").start()
 require("channels")
 require('sign_up_password.js')
 require('address_autofill.js')
+require('jquery.autoKana.js')
+require('autokana.js')
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
