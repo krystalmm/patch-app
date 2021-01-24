ecf162c80fabef34289409983dbb384b685bdcb4
krystalmm <taeyeon_aina@live.jp>
2021-01-24 21:01:36 +0900

    [add] validates_email_format_of gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功
+    else
+      render 'new'
+    end
+  end
+
   private
 
   def user_params
