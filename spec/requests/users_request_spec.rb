c14391a036c978309180a130e23c8476c519e7d6
krystalmm <taeyeon_aina@live.jp>
2021-01-25 17:04:00 +0900

    [add] success signup spec

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "POST /users" do
+    # attributes_forを使うのは、ハッシュとなるから、パラメータに値を渡すことができるから！
+    let(:user) { FactoryBot.attributes_for(:user) }
+    it "adds new user with correct signup information" do
+      aggregate_failures do
+        expect do
+          post users_path, params: { user: user }
+        end.to change(User, :count).by(1)
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to user_path(User.last)
+      end
+    end
+  end
 end
