9bbb95d7f86057c497cf6ed66ebc998afbc4bde5
krystalmm <taeyeon_aina@live.jp>
2021-01-26 16:50:29 +0900

    [add] sessions controller and sessions_request_spec

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   let(:user) { FactoryBot.create(:user) }
 
-  describe '#new' do
+  describe 'GET /signup' do
     it 'responds successfully' do
       get signup_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe '#show' do
+  describe 'GET /users/id' do
     it 'responds successfully' do
       get user_path(user)
       expect(response).to have_http_status(:success)
