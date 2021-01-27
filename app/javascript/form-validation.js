ef5dee51f3a106b21ad8833e2b2ef842c16961c1
krystalmm <taeyeon_aina@live.jp>
2021-01-27 20:36:54 +0900

    [add] backpage link

--- app/javascript/form-validation.js
+++ app/javascript/form-validation.js
@@ -1,11 +1,11 @@
 $(function() {
   $("#signup-form").validationEngine('attach', {
-    promptPosition: "bottomLeft"
+    promptPosition: "bottomLeft", scrollOffset: 140
   });
 });
 
 $(function() {
   $("#login-form").validationEngine('attach', {
-    promptPosition: "bottomLeft"
+    promptPosition: "bottomLeft", scrollOffset: 140
   });
 });
