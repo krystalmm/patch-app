25ad6ad4efea4cca80369e5b304bfd26e4e9e9c7
krystalmm <taeyeon_aina@live.jp>
2021-02-02 09:59:44 +0900

    [add] remember_token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
+      flash[:success] = 'ログインしました'
       redirect_to user
     else
       flash.now[:danger] = 'メールアドレスまたはパスワードの入力に誤りがあるか登録されていません'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 require 'nkf'
 
 class User < ApplicationRecord
-  attr_accessor :remamber_token
+  attr_accessor :remember_token
 
   # データベースに保存される前に全ての文字列を小文字に変換する！（大文字と小文字を区別しないため！）
   before_save { email.downcase! }
@@ -57,5 +57,15 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
