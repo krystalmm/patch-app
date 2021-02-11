f48d820767ce8290e58985c6157a239951b7297f
krystalmm <taeyeon_aina@live.jp>
2021-02-11 10:27:45 +0900

    [add] before_action to unsubscribe and withdraw

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
+
+  before_action :logged_in_user, only: [:unsubscribe, :withdraw]
+  before_action :correct_user, only: [:unsubscribe, :withdraw]
+
   def home
     # home
   end
@@ -23,6 +27,24 @@ class StaticPagesController < ApplicationController
     @user = User.find(params[:id])
     @user.update(is_valid: false)
     reset_session
+    flash[:primary] = 'またのご利用を心よりお待ちしております'
     redirect_to root_path
   end
+
+
+
+  private
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = 'ログインしてください'
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(user_path(current_user)) unless current_user?(@user)
+    end
 end
--- spec/requests/users_dismissed_request_spec.rb
+++ spec/requests/users_dismissed_request_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'UsersDismissed', type: :request do
-  let(:user) { FactoryBot.create(:user) }
   describe '#unsubscribe' do
+    let(:user) { FactoryBot.create(:user) }
+    before { log_in_as(user) }
+
     it 'responds successfully' do
       get confirm_unsubscribe_path(user)
       expect(response).to be_successful
@@ -11,9 +13,41 @@ RSpec.describe 'UsersDismissed', type: :request do
 
   describe '#withdraw' do
     let(:user) { FactoryBot.create(:user) }
+    before { log_in_as(user) }
+
     it 'succeeds dismissed' do
       patch withdraw_user_path(user)
       expect(response).to redirect_to root_url
+    end
+  end
+
+  describe 'before_action: :logged_in_user' do
+    let(:user) { FactoryBot.create(:user) }
+    it 'redirects unsubscribe when not logged in' do
+      get confirm_unsubscribe_path(user)
+      expect(response).to redirect_to login_url
+    end
+
+    it 'redirects withdraw when not logged in' do
+      patch withdraw_user_path(user)
+      expect(response).to redirect_to login_url
+    end
+  end
+
+  describe 'before_action: :correct_user' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:other_user) { FactoryBot.create(:user) }
+
+    before { log_in_as(other_user) }
+
+    it 'redirects unsubscribe when logged in as wrong user' do
+      get confirm_unsubscribe_path(user)
+      expect(response).to redirect_to user_path(other_user)
+    end
+
+    it 'redirects withdraw when logged in as wrong user' do
+      patch withdraw_user_path(user)
+      expect(response).to redirect_to user_path(other_user)
     end
   end
 end
