7f1851a051818dff85642d5a45acc2998a69635e
krystalmm <taeyeon_aina@live.jp>
2021-02-15 22:08:07 +0900

    [add] admin to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_121110) do
+ActiveRecord::Schema.define(version: 2021_02_15_124718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_02_15_121110) do
     t.boolean "is_valid", default: true, null: false
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  name: "Admin User",
+  email: "admin@example.com",
+  password: "password",
+  password_confirmation: "password",
+  name_kana: "アドミン　ユーザー",
+  postcode: "5300001",
+  prefecture_code: 27,
+  address_city: "大阪市北区梅田",
+  address_street: "",
+  address_building: "",
+  is_valid: true,
+  admin: true
+)
