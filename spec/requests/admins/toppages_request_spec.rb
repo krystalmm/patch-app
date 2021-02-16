c9d8da7b0998042dfb9de510ac60ea1c8dc38962
krystalmm <taeyeon_aina@live.jp>
2021-02-16 21:46:50 +0900

    [add] admins toppages spec

--- spec/requests/admins/toppages_request_spec.rb
+++ spec/requests/admins/toppages_request_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Admins::Toppages", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+  let(:admin_user) { FactoryBot.create(:user, :admin) }
 
   describe "GET /index" do
-    it "returns http success" do
-      get "/admins/toppages/index"
+    it "responds successfully as admin_user" do
+      log_in_as(admin_user)
+      get admins_root_path
       expect(response).to have_http_status(:success)
+    end
+
+    it 'redirects admins when user not admin' do
+      log_in_as(user)
+      get admins_root_path
+      expect(response).to redirect_to root_path
     end
   end
 
