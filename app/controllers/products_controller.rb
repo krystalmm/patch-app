4d62f6405ffaba2aff5e5a3caa181a6028f78eeb
krystalmm <taeyeon_aina@live.jp>
2021-02-20 14:28:26 +0900

    [update] index page to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all.order(:id)
+    if params[:option] == "hp"
+      @products = Product.all.order(price: "DESC")
+    elsif params[:option] == "cp"
+      @products = Product.all.order(price: "ASC")
+    else
+      @products = Product.all.order(:id)
+    end
   end
 
   def show
