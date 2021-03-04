5bb12844cc229c147a41de767530d13cc80749e1
krystalmm <taeyeon_aina@live.jp>
2021-03-04 15:38:46 +0900

    [add] google mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,10 +33,18 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
-  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:               587,
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            ENV['MAIL_USER_NAME'],
+    password:             ENV['MAIL_USER_PASSWORD'],
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  # config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
