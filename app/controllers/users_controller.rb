4742a3951ecd93b93a562268ac7a413771144a4f
krystalmm <taeyeon_aina@live.jp>
2021-03-09 17:46:21 +0900

    [update] responsive

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show, :edit, :update, :favorites]
-  before_action :correct_user, only: [:show, :edit, :update, :favorites]
+  before_action :correct_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
